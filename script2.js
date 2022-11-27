735c5cd6b6dd40650d22a581270a0aed13ae624f
eashirokovaa <eashirokovaa@gmail.com>
2022-11-27 16:48:43 +0300

    update

--- script2.js
+++ script2.js
@@ -9,26 +9,25 @@ class Cat {
     this.sex = sex;
   } 
 }
-
 function getValues() {
+  
   let nickname = document.getElementById("name");
   let name = document.getElementById("fio");
   let contacts = document.getElementById("contact");
 
   let selectElement = document.querySelector("#breed");
   let breed = selectElement.options[selectElement.selectedIndex].text;
+
   let checkboxes = document.getElementsByName("source");
-
   let food = "";
-
   for (var i = 0; i < checkboxes.length; i++) {
     if (checkboxes[i].checked) {
       food += checkboxes[i].value + "," + " ";
     }
   }
+
   let radio = document.getElementsByName("sex");
   let sex = "";
-
   for (i = 0; i < radio.length; i++) {
     if (radio[i].checked) {
       sex += radio[i].value;
